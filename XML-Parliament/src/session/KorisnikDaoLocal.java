46e19ce85b4d74267c6483187e300cb2a2aa5923
MilosSavic <savic.milan@sezampro.rs>
2016-05-22 16:33:25 +0200

    Something's wrong so I'm deleting the whole project

--- XML-Parliament/src/session/KorisnikDaoLocal.java
+++
@@ -1,8 +0,0 @@
-package session;
-
-import model.Korisnik;
-
-public interface KorisnikDaoLocal extends GenericDaoLocal<Korisnik, Integer> {
-
-	public Korisnik findKorisnikWithUsernameAndPassword(String username, String password);
-}
